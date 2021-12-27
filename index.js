ac9972ae61013c8383c311e9e238b46de8033e17
Kimberly Hayes <kshayes78@gmail.com>
2021-12-27 11:06:24 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let el = document.getElementById("main")
+el.remove();
+//document.body.append(elememt);
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"; 
